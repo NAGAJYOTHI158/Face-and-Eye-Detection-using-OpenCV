443b524412a57611460a894ffd6f3dd7cc120cad
Naga Jyothi Yarramreddy <129736217+NAGAJYOTHI158@users.noreply.github.com>
2024-01-02 09:51:16 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -12,7 +12,7 @@ while True:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),5)
         roi_gray = gray[y:y+w, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3,)
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3,5
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey),(ex+ew, ey+eh),(0,255,0),5)
 
